feedc22103bb97cb4d8be16df18ac048a3a40440
hoto17296 <mail@hoto.me>
2017-04-03 16:16:45 +0900

    update packages

--- setup/Brewfile.sh
+++ setup/Brewfile.sh
@@ -7,11 +7,9 @@ brew install zsh
 brew install zsh-completions
 brew install vim
 brew install git
-brew install tig
 brew install wget
 brew install tree
 brew install ag
-brew install go
 brew install mysql
 brew install awscli
 brew install mecab mecab-ipadic
@@ -28,13 +26,8 @@ brew install brew-cask
 
 brew cask install iterm2
 brew cask install java
-brew cask install firefox-ja
+brew cask install firefox --language=ja
 brew cask install google-chrome
 brew cask install google-japanese-ime
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install boot2docker
 brew cask install sequel-pro
-brew cask install vlc
-brew cask install karabiner
 brew cask install appcleaner

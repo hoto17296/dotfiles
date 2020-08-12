2cb81afb8c2a05745db90539ac02251103df0da8
hoto17296 <mail@hoto.me>
2020-08-12 13:54:19 +0900

    Back to Google Japanese IME

--- setup/Brewfile.sh
+++ setup/Brewfile.sh
@@ -22,6 +22,7 @@ brew cask install vivaldi
 brew cask install docker
 brew cask install java
 brew cask install google-chrome
+brew cask install google-japanese-ime
 brew cask install postico
 brew cask install appcleaner
 brew cask install insomnia

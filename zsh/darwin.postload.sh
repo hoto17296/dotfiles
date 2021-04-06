f76669f348a5e9d8759d1444345e19364679011b
hoto <mail@hoto.me>
2021-04-06 11:07:47 +0900

    fix PATH

--- zsh/darwin.postload.sh
+++ zsh/darwin.postload.sh
@@ -1,6 +1,6 @@
 setopt ignoreeof # Ctrl+D でログアウトしない
 
-export PATH="$(ghq root)/github.com/hoto17296/bin:$HOME/Library/Python/3.7/bin:/usr/local/opt/node@12/bin:$PATH"
+export PATH="$(ghq root)/github.com/hoto17296/bin:/usr/local/opt/node@14/bin:$PATH"
 export LANG=ja_JP.UTF-8
 
 export LSCOLORS=gxfxcxdxbxegedabagacag
